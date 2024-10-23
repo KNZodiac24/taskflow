a5f36ae15244446afeeb871254c2014bea1a3984
KNZodiac24 <navakenny_16@hotmail.com>
2024-10-23 00:34:32 -0500

    Terminada la vista de ventana para Agregar Tarea

--- src/vista/JDAgregarTarea.java
+++ src/vista/JDAgregarTarea.java
@@ -1,23 +1,27 @@
 package vista;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class JDAgregarTarea extends JDialog {
     private JTextField jTFNombreTarea;
     private JTextArea jTADescripcionTarea;
-    private JTextField jTFFechaCuliminacion;
+    private JTextField jTFFechaCulminacion;
     public JButton jBAgregarTarea;
 
     public JDAgregarTarea(){
         this.setTitle("Agregar tarea");
-        this.getContentPane().setPreferredSize(new Dimension(500, 500));
+        this.getContentPane().setPreferredSize(new Dimension(500, 410));
         this.getContentPane().setLayout(null);
         
         initComponentes();
@@ -33,12 +37,64 @@ public class JDAgregarTarea extends JDialog {
         // Para agregar el nombre
         JLabel jLNombreTarea = new JLabel("Nombre:");
         jLNombreTarea.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 20));
-        jLNombreTarea.setBounds(30, 30, 100, 20);
+        jLNombreTarea.setBounds(30, 30, 100, 25);
         this.add(jLNombreTarea);
 
         this.jTFNombreTarea = new JTextField();
-        this.jTFNombreTarea.setBounds(125, 30, 200, 20);
+        this.jTFNombreTarea.setBounds(110, 30, 360, 25);
+        this.jTFNombreTarea.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 16));
         this.add(this.jTFNombreTarea);
+
+        // Para agregar la descripción
+        JLabel jLDescripcion = new JLabel("Descripción:");
+        jLDescripcion.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 20));
+        jLDescripcion.setBounds(30, 65, 150, 25);
+        this.add(jLDescripcion);
+
+        this.jTADescripcionTarea = new JTextArea();
+        this.jTADescripcionTarea.setBounds(30, 100, 440, 200);
+        this.jTADescripcionTarea.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 16));
+        this.jTADescripcionTarea.setWrapStyleWord(true);
+        this.jTADescripcionTarea.setLineWrap(true);
+        JScrollPane jSPScrollDescripcion = new JScrollPane(this.jTADescripcionTarea);
+        jSPScrollDescripcion.setBounds(this.jTADescripcionTarea.getBounds()); 
+        this.add(jSPScrollDescripcion);
+
+        // Para agregar la fecha de culminación
+        JLabel jLFechaCulminacion = new JLabel("Fecha de culminación:");
+        jLFechaCulminacion.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 20));
+        jLFechaCulminacion.setBounds(30, 310, 200, 25);
+        this.add(jLFechaCulminacion);
+
+        this.jTFFechaCulminacion = new JTextField("dd/mm/aaaa");
+        this.jTFFechaCulminacion.setBounds(230, 310, 240, 25);
+        this.jTFFechaCulminacion.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 16));
+        this.jTFFechaCulminacion.setForeground(Color.GRAY);
+        this.jTFFechaCulminacion.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (jTFFechaCulminacion.getText().equals("dd/mm/aaaa")) {
+                    jTFFechaCulminacion.setText("");
+                    jTFFechaCulminacion.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (jTFFechaCulminacion.getText().isEmpty()) {
+                    jTFFechaCulminacion.setText("dd/mm/aaaa");
+                    jTFFechaCulminacion.setForeground(Color.GRAY);
+                }
+            }
+        });
+        this.add(this.jTFFechaCulminacion);
+
+        // Para el botón de agregar tarea
+        this.jBAgregarTarea = new JButton("Agregar tarea");
+        this.jBAgregarTarea.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 16));
+        this.jBAgregarTarea.setBounds(180, 355, 130, 40);
+        this.jBAgregarTarea.setFocusable(false);
+        this.add(this.jBAgregarTarea);
     }
 
     public void mostrar(){
